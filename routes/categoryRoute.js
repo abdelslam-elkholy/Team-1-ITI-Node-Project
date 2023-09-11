e409bc009773db69475ee7facce86798faa4cc19
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-09-11 20:50:57 +0300

    update

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const categoryCountroller = require("./../controlleres/categoryController");
+const authMiddleware = require("./../middlewares/authMiddleware");
 
-router.route("/signup").post(authCountroller.signUp);
-router.route("/signin").post(authCountroller.signIn);
+router
+  .route("/category")
+  .get(categoryCountroller.getCategories)
+  .post(authMiddleware.protect, categoryCountroller.createCategory);
+router
+  .route("/category/:id")
+  .get(categoryCountroller.getCategory)
+  .patch(authMiddleware.protect, categoryCountroller.updateCategory)
+  .delete(authMiddleware.protect, categoryCountroller.deleteCategory);
 
 module.exports = router;
