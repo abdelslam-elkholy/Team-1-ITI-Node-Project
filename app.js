44f8fb7832270b52348a542b5fb54573ef599650
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-09-12 05:05:22 +0300

    update

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const { errorHandler, AppError } = require("./controlleres/errorController");
-const hostRoute = require("./routes/hostRoute");
+const houseRoute = require("./routes/houseRoute");
 const userRoute = require("./routes/userRoute");
+const reservationRoute = require("./routes/reservationRoute");
 
 const app = express();
 app.use(express.json());
 
-app.use("/host", hostRoute);
+app.use("/house", houseRoute);
 app.use("/user", userRoute);
+app.use("/reservation", reservationRoute);
 
 app.all("*", (req, res, next) => {
   const err = new AppError(`there is nou route for this ${req.originalUrl}`);
--- routes/reservationRoute.js
+++ routes/reservationRoute.js
@@ -1,90 +1,17 @@
-const fs = require("fs");
 const express = require("express");
 const router = express.Router();
-// const authentication = require("../middlewares/authentication");
-var {
-    getAllReservations,
-    saveReservation,
-    getReservationById,
-    updateReservationById,
-    deleteReservationById,
-} = require("../controlleres/ReservationController");
-const { error } = require("console");
-//==============================================
-// router.use(authentication);
-// get all Reservations
+const reservationController = require("./../controllers/reservationController");
+const authMiddleware = require("./../middlewares/authMiddleware");
 
-router.get("/", async (req, res) => {
-  try {
-    var Reservations = await getAllReservations();
-    res.json({ data: Reservations });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+router
+  .route("/")
+  .get(reservationController.getReservations)
+  .post(reservationController.createReservation);
+router
+  .route("/:id")
+  .get(reservationController.getReservation)
+  .patch(reservationController.updateReservation)
+  .delete(reservationController.deleteReservation);
 
-// save Reservation
-
-router.post("/",  async (req, res) => {
-  var hostId = req.body.hostId;
-  var userId = req.body.userId;
-  var checkinDate = req.body.checkinDate;
-  var checkoutDate = req.body.checkoutDate;
-  var price = req.body.price;
-
-  try {
-    var newReservation = await saveReservation({userId:userId, price:price,checkoutDate:checkoutDate,checkinDate:checkinDate,hostId: hostId});
-   
-    res.status(201).json({ data: newReservation });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-//get Reservation by id
-
-router.get("/:id", async (req, res) => {
-  var { id } = req.params;
-  try {
-    var newReservation = await getReservationById(id);
-    if (newReservation) {
-      res.status(201).json({ data: newReservation });
-    } else {
-      res.status(404).json({ message: "Not found" });
-    }
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-// update newReservation by id
-router.patch("/:id", async (req, res) => {
-  var { hostId, checkinDate ,checkoutDate,price} = req.body;
-  var { id } = req.params;
-  try {
-    var Reservation = await updateReservationById(id, hostId, checkinDate,checkoutDate,price);
-    if (Reservation) {
-      res.status(200).json({ data: Reservation });
-    } else {
-      res.status(404).json({ message: "Not found" });
-    }
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-// delete Reservation by id
-router.delete("/:id", async (req, res) => {
-  var { id } = req.params;
-  try {
-    var newReservation = await deleteReservationById(id);
-    if (newReservation) {
-      res.status(201).json({ message: "Deleted", data: newReservation });
-    } else {
-      res.status(404).json({ message: "Not found" });
-    }
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-module.exports = router;
+router.route("/user/:id").get(reservationController.getUserReservations);
+router.route("/house/:id").get(reservationController.getHouseReservations);
