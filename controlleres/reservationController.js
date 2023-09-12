bc62f735008d325dde1f40159ca6dff98ea9e98f
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-09-12 04:53:59 +0300

    update

--- controlleres/reservationController.js
+++ controlleres/reservationController.js
@@ -1,36 +1,110 @@
-const fs = require("fs");
-var ReservationModel = require("../models/reservationModel");
-function getAllReservations() {
-  return ReservationModel.find().populate("hostId", "userId");
-}
+const Reservation = require("../models/reservationModel");
+const { AppError } = require("../utils/appError");
 
-function saveReservation(Reservation) {
-  return ReservationModel.create(Reservation);
-}
+exports.getAllReservations = async (req, res, next) => {
+  try {
+    const reservations = await Reservation.find();
 
-function getReservationById(id) {
-  return ReservationModel.findOne({ _id: id }).populate("hostId", "userId");
-}
+    res.status(200).json({
+      status: "success",
+      reservationCount: reservations.length,
+      data: {
+        reservations,
+      },
+    });
+  } catch (error) {
+    return next(new AppError(error.message, 500));
+  }
+};
+exports.createReservation = async (req, res, next) => {
+  try {
+    const newReservation = await Reservation.create(req.body);
 
-function updateReservationById(id, checkinDate, checkoutDate) {
-  return ReservationModel.findByIdAndUpdate(
-    id,
-    { checkinDate: checkinDate, checkoutDate: checkoutDate },
-    { new: true }
-  ).populate("hostId", "userId");
-}
+    res.status(201).json({
+      status: "success",
+      data: {
+        reservation: newReservation,
+      },
+    });
+  } catch (error) {
+    return next(new AppError(error.message, 500));
+  }
+};
 
-function deleteReservationById(id) {
-  return ReservationModel.findByIdAndDelete({ _id: id }).populate(
-    "hostId",
-    "userId"
-  );
-}
-//================================================
-module.exports = {
-  getAllReservations,
-  saveReservation,
-  getReservationById,
-  updateReservationById,
-  deleteReservationById,
+exports.getReservation = async (req, res, next) => {
+  try {
+    const reservation = await Reservation.findById(req.params.id);
+
+    if (!reservation) {
+      return next(
+        new AppError(
+          `There is no reservation with the id ${req.params.id}`,
+          404
+        )
+      );
+    }
+
+    res.status(200).json({
+      status: "Success",
+      data: {
+        reservation,
+      },
+    });
+  } catch (error) {
+    return next(new AppError(error.message, 500));
+  }
+};
+
+exports.updateReservation = async (req, res, next) => {
+  try {
+    const reservation = await Reservation.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+      }
+    );
+
+    if (!reservation) {
+      return next(
+        new AppError(
+          `There is no reservation with the id ${req.params.id}`,
+          404
+        )
+      );
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        reservation,
+      },
+    });
+  } catch (error) {
+    return next(new AppError(error.message, 500));
+  }
+};
+
+exports.deleteReservation = async (req, res, next) => {
+  try {
+    const reservation = await Reservation.findByIdAndDelete(req.params.id);
+
+    if (!reservation) {
+      return next(
+        new AppError(
+          `There is no reservation with the id ${req.params.id}`,
+          404
+        )
+      );
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        reservation,
+      },
+    });
+  } catch (error) {
+    return next(new AppError(error.message, 500));
+  }
 };
