c9cc4b8863bc7b18ce12f9d68698a2ba1da554d3
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-09-13 09:35:25 +0300

    update

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,6 +4,17 @@ const authCountroller = require("./../controlleres/authController");
 
 router.route("/signup").post(authCountroller.signUp);
 router.route("/signin").post(authCountroller.signIn);
+router.route("/signout").get(authCountroller.signOut);
 
+router.route("/resetPassword/:token").patch(authCountroller.resetPassword);
+router.route("/updatePassword").patch(authCountroller.updatePassword);
 router.route("/forgotPassword").post(authCountroller.forgotPassword);
+
+router
+  .route("/updateMe")
+  .patch(authCountroller.protect, authCountroller.updateMe);
+router
+  .route("/deleteMe")
+  .delete(authCountroller.protect, authCountroller.deleteMe);
+
 module.exports = router;
