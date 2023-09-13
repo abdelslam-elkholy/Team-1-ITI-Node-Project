e805469eb72ec2d60af4f0bb9e3a462d9d7cf2d4
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-09-14 02:14:46 +0300

    update

--- routes/wishlistRoute.js
+++ routes/wishlistRoute.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const wishlistController = require("../controlleres/wishlistController");
 const authController = require("./../controlleres/authController");
+
 router
   .route("/")
   .get(wishlistController.getAllWishlists)
   .post(authController.protect, wishlistController.createWishlist);
+
 router
   .route("/:id")
   .get(wishlistController.getWishlist)
