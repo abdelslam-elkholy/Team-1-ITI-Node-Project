a0dcf7b91b9933ab4b6cebb52ae70bba8031af1a
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-09-23 07:57:02 +0300

    update

--- models/houseModel.js
+++ models/houseModel.js
@@ -1,60 +1,65 @@
 const mongoose = require("mongoose");
 
-const houseSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minLength: [5, "title is less than 3 characters"],
-    maxLength: [80, "title is more than 80 characters"],
-    required: true,
-    trim: true,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  currencySymbol: {
-    type: String,
-    enum: ["$", "€", "EGP"],
-    default: "$",
-    required: false,
-  },
-  numberOfRooms: {
-    type: Number,
-    required: true,
-  },
-  numberOfBedrooms: {
-    type: Number,
-    required: true,
-  },
-  guestNumber: {
-    type: Number,
-    required: true,
-  },
-  rate: {
-    type: Number,
-  },
-  description: {
-    type: String,
-  },
+const houseSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      minLength: [5, "title is less than 3 characters"],
+      maxLength: [80, "title is more than 80 characters"],
+      required: true,
+      trim: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    currencySymbol: {
+      type: String,
+      enum: ["$", "€", "EGP"],
+      default: "$",
+      required: false,
+    },
+    numberOfRooms: {
+      type: Number,
+      required: true,
+    },
+    numberOfBedrooms: {
+      type: Number,
+      required: true,
+    },
+    guestNumber: {
+      type: Number,
+      required: true,
+    },
+    rate: {
+      type: Number,
+    },
+    description: {
+      type: String,
+    },
 
-  images: {
-    type: [String],
-    required: true,
-  },
+    images: {
+      type: [String],
+      required: true,
+    },
 
-  category: {
-    type: String,
-    required: true,
+    category: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "User",
+    },
   },
-  userId: {
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: "User",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const House = mongoose.model("house", houseSchema);
 
