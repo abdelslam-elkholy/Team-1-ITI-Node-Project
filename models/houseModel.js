21eabebb3f7b733fdb9990f0f3f3016b1967a629
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-10-03 13:51:57 +0300

    update

--- models/houseModel.js
+++ models/houseModel.js
@@ -102,21 +102,6 @@ houseSchema.methods.calculateUnavailableDates = async function () {
   return unavailableDates;
 };
 
-houseSchema.post(/^find/, async function (req, res, next) {
-  if (!req.user) {
-    return next();
-  }
-  const inwishlist = await mongoose
-    .model("wishlist")
-    .findOne({ userId: req.user._id, houseId: this._id });
-  if (inwishlist) {
-    this.inwishlist = true;
-  } else {
-    this.inwishlist = false;
-  }
-  next();
-});
-
 const House = mongoose.model("house", houseSchema);
 
 module.exports = House;
